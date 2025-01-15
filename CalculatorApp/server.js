e66e656ba4015a4b3cc82039199866e9bfdc4cf9
Bakame <137406652+Bakame03@users.noreply.github.com>
2025-01-15 12:43:41 +0200

    Add files via upload

---
+++ CalculatorApp/server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('Error loading index.html');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        }
+    });
+});
+
+const PORT = process.env.PORT || 5500;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
